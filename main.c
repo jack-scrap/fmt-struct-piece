d4ac28eddd23df3c71633381c09667b217aa1116
jackhasakeyboard <jackhasakeyboard@gmail.com>
2024-08-11 04:50:07 -0600

    Output artist and creation separated by a dash, "art" is generic and can apply to anything

--- main.c
+++ main.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
+const char infix = '-';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("%s %c %s", argv[1], infix, argv[1 + 1]);
+
 	return 0;
 }
